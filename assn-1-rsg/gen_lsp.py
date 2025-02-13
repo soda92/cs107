172e313f7015b97415210bb490e778347e5584d3
sodacris <wjxa20152015@gmail.com>
2025-02-13 22:11:47 +0800

    fix clangd

--- assn-1-rsg/gen_lsp.py
+++ assn-1-rsg/gen_lsp.py
@@ -1,14 +1,50 @@
 from pathlib import Path
 import json
-from sodatools import read_path, write_path
+import os
+import functools
+from sodatools import read_path, write_path, str_path
 
 CURRENT = Path(__file__).resolve().parent
 
 commands = CURRENT.joinpath("build").joinpath("compile_commands.json")
+commands2 = commands.parent.joinpath(commands.stem)
+if commands.exists():
+    commands.rename(commands2)
+commands = commands2
+
+
+def which(name):
+    path = os.environ["PATH"]
+    for p in path.split(";"):
+        exe = Path(p).joinpath(name)
+        if exe.exists():
+            return exe
+
+
+@functools.cache
+def get_gcc_path():
+    out = which("gcc.exe")
+    return out.resolve().parent.parent
+
+
+def get_includes():
+    ret = []
+    includes = [
+        "{gcc}include/c++/13.2.0",
+        "{gcc}x86_64-w64-mingw32/include",
+        "{gcc}include/c++/13.2.0/x86_64-w64-mingw32",
+    ]
+    for i in includes:
+        i = i.replace("{gcc}", str_path(get_gcc_path()) + "/")
+        ret.append("-I" + i)
+    return ret
 
 
 def fix_command(c) -> str:
-    print(c)
+    list_ = c.replace("\\", "/").split()
+    list_.extend(get_includes())
+
+    return " ".join(list_)
 
 
 def fix_commands():
