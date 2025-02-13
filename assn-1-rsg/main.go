cf9bf04a15fcb5a889779006ea48ac72ce60ec2f
sodacris <wjxa20152015@gmail.com>
2025-02-14 01:12:20 +0800

    add gui

--- assn-1-rsg/main.go
+++ assn-1-rsg/main.go
@@ -5,12 +5,35 @@ package main
 // #include <stdlib.h>
 // #include "main.h"
 import "C"
-import "unsafe"
-import "fmt"
+import (
+	"unsafe"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
 
-func main() {
+func get_output() string {
 	cStr := C.rsg_main(C.CString("data/excuse.g"))
 	goStr := C.GoString(cStr)
-	fmt.Print(goStr)
 	C.free(unsafe.Pointer(cStr))
+	return goStr
+}
+
+func makeUI() (*widget.Label, *widget.Entry) {
+	out := widget.NewLabel(get_output())
+	in := widget.NewEntry()
+
+	in.OnChanged = func(content string) {
+		out.SetText("Hello " + content + "!")
+	}
+	return out, in
+}
+
+func main() {
+	a := app.New()
+	w := a.NewWindow("Assignment 1")
+	w.CenterOnScreen()
+
+	w.SetContent(container.NewVBox(makeUI()))
+	w.ShowAndRun()
 }
