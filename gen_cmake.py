f717dc3a49ba06b4c1f0d300e36507d972d4b291
sodacris <wjxa20152015@gmail.com>
2025-02-16 10:27:44 +0800

    add user presets

--- gen_cmake.py
+++ gen_cmake.py
@@ -10,33 +10,38 @@ CURRENT = Path(__file__).resolve().parent
 sys.path.insert(0, str_path(CURRENT))
 
 
-def get_cmake_templates() -> list[Path]:
-    return get_glob_files("**/CML.template")
-
-
-def which(name) -> Path:
-    "find executable by name from PATH"
-    path = os.environ["PATH"]
-    for p in path.split(";"):
-        exe = Path(p).joinpath(name)
-        if exe.exists():
-            return exe
-    logging.fatal(f"{name} not found in PATH")
-    exit(-1)
-
-
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--gcc", action="store_true", help="use gcc in PATH")
-    parser.add_argument("--tdm", action="store_true", help="use tdm gcc")
-    args = parser.parse_args()
-    for template in get_cmake_templates():
-        target = template.parent.joinpath("CMakeLists.txt")
-        s = read_path(template)
-        if args.tdm:
-            s = s.replace("g++", "C:/TDM-GCC-64/bin/g++.exe")
-            s = s.replace("gcc", "C:/TDM-GCC-64/bin/gcc.exe")
-        else:
-            s = s.replace("g++", which("g++"))
-            s = s.replace("gcc", which("gcc"))
+    for template in get_glob_files("**/CMakePresets.json"):
+        target = template.parent.joinpath("CMakeUserPresets.json")
+        s = """
+{
+  "version": 10,
+  "cmakeMinimumRequired": {
+    "major": 3,
+    "minor": 23,
+    "patch": 0
+  },
+  "configurePresets": [
+    {
+      "name": "windows2",
+      "inherits": "windows-only",
+      "displayName": "Windows-only configuration",
+      "cacheVariables": {
+        "CMAKE_CXX_COMPILER": {
+          "type": "STRING",
+          "value": "C:/TDM-GCC-64/bin/g++.exe"
+        },
+        "CMAKE_C_COMPILER": "C:/TDM-GCC-64/bin/gcc.exe",
+        "SECOND_CACHE_VARIABLE": "ON"
+      }
+    }
+  ],
+  "buildPresets": [
+    {
+      "name": "windows2",
+      "configurePreset": "windows2"
+    }
+  ]
+}
+        """
         write_path(target, s)
